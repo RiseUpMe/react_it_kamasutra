bc6437ba2ee277e99af230362cf0e0278242a7bc
RiseUpMe <zenek7@mailru>
2022-04-19 14:55:11 +0300

    it-kamasutra_my_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsYcoRjP4PUOjV_S-j9cSfD8C3RyK1JVOVmQ&usqp=CAU'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a> Menu</a>
+                </div>
+                <div>
+                    <a> Messages</a>
+                </div>
+                <div>
+                    <a> News</a>
+                </div>
+                <div>
+                    <a> Music</a>
+                </div>
+                <div>
+                    <a> Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://copyblogger.com/cdn-origin/images/long_road_to_nowhere.jpg'/>
+                </div>
+                <div>
+                    ava+description
+                    <img src='https://img.myloview.com/posters/rocket-logo-design-template-idea-700-190811052.jpg'/>
+                </div>
+                <div>
+                    My post
+                    <div>
+                        new post
+                    </div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+
+
+            </div>
+        </div>);
 }
 
 export default App;
